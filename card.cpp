66370c7212455c7279b1fd403b800a374d2b7c7c
Matt Berry <matthewjberry03@gmail.com>
2025-05-02 16:06:24 -0600

    created constructor in card.cpp

--- card.cpp
+++ card.cpp
@@ -1,1 +1,12 @@
 /* Created by Matt Berry */
+
+#include "card.h"
+
+/* Initialize card 
+ * Pretty much the only method we need here */
+Card::Card(Value v, Suit s)
+  : value(v)
+  , suit(s)     
+{}
+
+// Really not sure how I feel about this syntax
