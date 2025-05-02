33bcf7bac192a47923fa5dd03d702781b509504d
Matt Berry <matthewjberry03@gmail.com>
2025-05-02 15:56:13 -0600

    create value enum for cards

---
+++ value.h
@@ -0,0 +1,18 @@
+/* Create by Matt Berry */
+
+/* Represents the face value of a playing card */
+enum class Value : unsigned {
+  Ace = 1,
+  Two,
+  Three,
+  Four,
+  Five,
+  Six,
+  Seven,
+  Eight,
+  Nine,
+  Ten,
+  Jack,
+  Queen,
+  King
+};
