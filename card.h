ac9801a5e0ad19de446a8bd584e900a231d33d47
Matt Berry <matthewjberry03@gmail.com>
2025-05-02 15:57:07 -0600

    created card header with all the fields I think ill need

--- card.h
+++ card.h
@@ -1,1 +1,22 @@
 /* Created by Matt Berry */
+
+#ifndef CARD_H
+#define CARD_H
+
+#include "suit.h"
+#include "value.h"
+
+/* Represents a single playing card */
+class Card {
+
+public:
+  // Constructor
+  Card(Value v, Suit s);
+
+  // Face Value
+  Value value;
+  // Suit
+  Suit suit;
+};
+
+#endif
