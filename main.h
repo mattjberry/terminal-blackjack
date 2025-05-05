6d5d5081d644950711c43a6cc1be5fe5af2924ef
Matt Berry <matthewjberry03@gmail.com>
2025-05-05 17:15:35 -0600

    created header for main to simplify including

---
+++ main.h
@@ -0,0 +1,14 @@
+/* Created by Matt Berry
+ * Header file to simplify includes from main.cpp */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "suit.h"
+#include "value.h"
+#include "deck.h"
+
+#include <iostream>
+using namespace std;
+
+#endif
