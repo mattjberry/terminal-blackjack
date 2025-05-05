4a6a3c97941a494ae5f47c4ed81c4c9958637209
Matt Berry <matthewjberry03@gmail.com>
2025-05-05 17:13:59 -0600

    implemented header file for deck for now, adding guards, constructor, dealCard and vector to store deck in

--- deck.h
+++ deck.h
@@ -1,1 +1,22 @@
 /* Created by Matt Berry */
+
+#ifndef DECK_H
+#define DECK_H
+
+#include <vector>
+#include <random>
+#include <chrono>
+#include "card.h"
+
+/* Represents a deck of standard playing cards (no jokers) */
+class Deck {
+
+  public:
+    Deck();
+    Card dealCard();
+
+  private:
+    std::vector<Card> cards;
+};
+
+#endif
